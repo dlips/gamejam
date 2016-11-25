dc961ffd23273fd2e4bd4e3b5559653f1167ad55
shortneck <mitohnescharfesauce@web.de>
2016-11-25 22:15:27 +0100

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,20 @@ var myState = {
     create : function () {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.add.sprite(0, 0, 'background');
-
+        
+        //männchen erstellen
         people = game.add.group();
         people.enableBody = true;
-
+        var man = people.create(10, game.world.height - 350, 'man');
+        
+        //plattformen erstellen
         platform = game.add.group();
         platform.enableBody = true;
         var startplatform = platform.create(10,10,'platform');
         startplatform.body.immovable = true;
 
 
-        var man = people.create(10, game.world.height - 350, 'man');
+        
         man.scale.setTo(3,3);
     },
     update : function () {
