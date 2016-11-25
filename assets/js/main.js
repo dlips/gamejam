a70b45716423fab105da838bed47d0b1e4dfda87
dlips <mail@dlips.de>
2016-11-26 00:07:55 +0100

    Zielfunktion animation 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ var myState = {
         game.load.image('background', 'assets/img/background.png');
         game.load.image('man', 'assets/img/man.png');
         game.load.image('platform', 'assets/img/simpleplatform.png');
-        game.load.spritesheet('testanimation', 'assets/img/testanimation.png', 51, 51, 30);
+        game.load.spritesheet('testanimation', 'assets/img/testanimation.png', 32, 32, 10);
+        game.load.spritesheet('crosshair', 'assets/img/circle.png', 50, 50, 6);
     },
     create : function () {
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -22,9 +23,13 @@ var myState = {
         var startplatform = platform.create(10,100,'platform');
         startplatform.body.immovable = true;
 
-        var testanimation = game.add.sprite(150, 150, 'testanimation');
-        testanimation.animations.add('walk');
-        testanimation.animations.play('walk', 2, true);
+        //var testanimation = game.add.sprite(150, 150, 'testanimation');
+        //testanimation.animations.add('walk');
+        //testanimation.animations.play('walk', 2, true);
+    
+        var testanimation2 = game.add.sprite(150, 150, 'crosshair');
+        testanimation2.animations.add('move');
+        testanimation2.animations.play('move', 2, true);
     },
     update : function () {
 
