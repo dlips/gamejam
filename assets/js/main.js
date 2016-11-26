d04cf4b94169eddd3951d238a7e00168f885fa13
dlips <mail@dlips.de>
2016-11-26 01:31:25 +0100

    Teleport verbessert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 var myState = {
+    aimAngularVelocity : (90 / 3),
+    teleportMaxRange : 200,
+    aimRangeSpeed : (200 / 3),
     preload :  function () {
         game.load.image('background', 'assets/img/background.png');
         game.load.image('man', 'assets/img/man.png');
@@ -11,22 +14,21 @@ var myState = {
 
         var background = game.add.sprite(0, 0, 'background');
         background.inputEnabled = true;
-        background.events.onInputDown.add(this.teleport, this);
-        this.people = game.add.group();
-        this.people.enableBody = true;
-        var man = this.people.create(10,10,'man');
-       // man.scale.setTo(3,3);
-
+        background.events.onInputDown.add(function() {
+            var x = game.input.x;
+            var y = game.input.y;
+            this.teleport(x, y);
+        }, this);
+        
+        this.player = game.add.sprite(10,10,'man');
+        this.player.enableBody = true;
+       
         //plattformen erstellen
         platform = game.add.group();
         platform.enableBody = true;
         var startplatform = platform.create(10,100,'platform');
         startplatform.body.immovable = true;
 
-        //var testanimation = game.add.sprite(150, 150, 'testanimation');
-        //testanimation.animations.add('walk');
-        //testanimation.animations.play('walk', 2, true);
-    
         var testanimation2 = game.add.sprite(150, 150, 'crosshair');
         testanimation2.animations.add('move');
         testanimation2.animations.play('move', 2, true);
@@ -34,13 +36,9 @@ var myState = {
     update : function () {
 
     },
-    teleport : function() {
-        var x = game.input.x;
-        var y = game.input.y;
-        this.people.destroy();
-        this.people = game.add.group();
-        this.people.enableBody = true;
-        var man = this.people.create(x, y, 'man');
+    teleport : function(x, y) {
+        this.player.x = x;
+        this.player.y = y;
     }
 };
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', myState);
