f17134ae906adeebec253366a5acf65c75b4b799
shortneck <mitohnescharfesauce@web.de>
2016-11-27 20:26:15 +0100

    aaa

--- assets/js/main.js
+++ assets/js/main.js
@@ -406,7 +406,7 @@ var Load = {
     preload : function(){
         
         game.stage.backgroundColor = 0xffffff;
-        var loadingLabel = game.add.text(400,300,'loading ...',{font: '20px Courier', fill: '#fffff'});
+        var loadingLabel = game.add.text(400,300,'loading ... dman sucks',{font: '20px Courier', fill: '#fffff'});
 
         game.load.image('menuscreen','assets/img/menu.png');    
         game.load.image('menumoon','assets/img/menumoon.png');
